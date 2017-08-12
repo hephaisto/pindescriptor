92fdd50dc47290b7c24f10aa42ee3cdbb2de6ae3
hephaisto <hephaisto@users.noreply.github.com>
2017-08-12 17:47:07 +0200

    Moved all code from .c to .h file

--- macros/ATmega328.h
+++ macros/ATmega328.h
@@ -72,98 +72,540 @@
 #define TCR2BUB_reg ASSR
 #define OCIE2B_reg TIMSK2
 #define SPR01_reg SPCR0
+
 // timer0_mode
-inline void timer0_mode_normal();
-inline void timer0_mode_pwm_phasecorrect_until_0xFF();
-inline void timer0_mode_ctc();
-inline void timer0_mode_fast_pwm_until_0xFF();
-inline void timer0_mode_pwm_phasecorrect_until_OCR0A();
-inline void timer0_mode_fast_pwm_until_OCR0A();
+
+inline void timer0_mode_normal()
+{
+	TCCR0B &= ~(1<<WGM02);
+	TCCR0A &= ~(1<<WGM01);
+	TCCR0A &= ~(1<<WGM00);
+}
+
+inline void timer0_mode_pwm_phasecorrect_until_0xFF()
+{
+	TCCR0B &= ~(1<<WGM02);
+	TCCR0A &= ~(1<<WGM01);
+	TCCR0A |=  (1<<WGM00);
+}
+
+inline void timer0_mode_ctc()
+{
+	TCCR0B &= ~(1<<WGM02);
+	TCCR0A |=  (1<<WGM01);
+	TCCR0A &= ~(1<<WGM00);
+}
+
+inline void timer0_mode_fast_pwm_until_0xFF()
+{
+	TCCR0B &= ~(1<<WGM02);
+	TCCR0A |=  (1<<WGM01);
+	TCCR0A |=  (1<<WGM00);
+}
+
+inline void timer0_mode_pwm_phasecorrect_until_OCR0A()
+{
+	TCCR0B |=  (1<<WGM02);
+	TCCR0A &= ~(1<<WGM01);
+	TCCR0A |=  (1<<WGM00);
+}
+
+inline void timer0_mode_fast_pwm_until_OCR0A()
+{
+	TCCR0B |=  (1<<WGM02);
+	TCCR0A |=  (1<<WGM01);
+	TCCR0A |=  (1<<WGM00);
+}
+
 // timer0_clockselect
-inline void timer0_clockselect_none();
-inline void timer0_clockselect_io_1();
-inline void timer0_clockselect_io_8();
-inline void timer0_clockselect_io_64();
-inline void timer0_clockselect_io_256();
-inline void timer0_clockselect_io_1024();
-inline void timer0_clockselect_on_falling_T0();
-inline void timer0_clockselect_on_rising_T0();
+
+inline void timer0_clockselect_none()
+{
+	TCCR0B &= ~(1<<CS02);
+	TCCR0B &= ~(1<<CS01);
+	TCCR0B &= ~(1<<CS00);
+}
+
+inline void timer0_clockselect_io_1()
+{
+	TCCR0B &= ~(1<<CS02);
+	TCCR0B &= ~(1<<CS01);
+	TCCR0B |=  (1<<CS00);
+}
+
+inline void timer0_clockselect_io_8()
+{
+	TCCR0B &= ~(1<<CS02);
+	TCCR0B |=  (1<<CS01);
+	TCCR0B &= ~(1<<CS00);
+}
+
+inline void timer0_clockselect_io_64()
+{
+	TCCR0B &= ~(1<<CS02);
+	TCCR0B |=  (1<<CS01);
+	TCCR0B |=  (1<<CS00);
+}
+
+inline void timer0_clockselect_io_256()
+{
+	TCCR0B |=  (1<<CS02);
+	TCCR0B &= ~(1<<CS01);
+	TCCR0B &= ~(1<<CS00);
+}
+
+inline void timer0_clockselect_io_1024()
+{
+	TCCR0B |=  (1<<CS02);
+	TCCR0B &= ~(1<<CS01);
+	TCCR0B |=  (1<<CS00);
+}
+
+inline void timer0_clockselect_on_falling_T0()
+{
+	TCCR0B |=  (1<<CS02);
+	TCCR0B |=  (1<<CS01);
+	TCCR0B &= ~(1<<CS00);
+}
+
+inline void timer0_clockselect_on_rising_T0()
+{
+	TCCR0B |=  (1<<CS02);
+	TCCR0B |=  (1<<CS01);
+	TCCR0B |=  (1<<CS00);
+}
+
+// timer0_out_A
+
+inline void timer0_out_A_no_pwm()
+{
+	TCCR0A &= ~(1<<COM0A1);
+	TCCR0A &= ~(1<<COM0A0);
+}
+
+inline void timer0_out_A_clear_on_compare_match()
+{
+	TCCR0A |=  (1<<COM0A1);
+	TCCR0A &= ~(1<<COM0A0);
+}
+
+inline void timer0_out_A_set_on_compare_match()
+{
+	TCCR0A |=  (1<<COM0A1);
+	TCCR0A |=  (1<<COM0A0);
+}
+
+// timer0_out_B
+
+inline void timer0_out_B_no_pwm()
+{
+	TCCR0A &= ~(1<<COM0B1);
+	TCCR0A &= ~(1<<COM0B0);
+}
+
+inline void timer0_out_B_clear_on_compare_match()
+{
+	TCCR0A |=  (1<<COM0B1);
+	TCCR0A &= ~(1<<COM0B0);
+}
+
+inline void timer0_out_B_set_on_compare_match()
+{
+	TCCR0A |=  (1<<COM0B1);
+	TCCR0A |=  (1<<COM0B0);
+}
+
 // timer1_mode
-inline void timer1_mode_normal();
-inline void timer1_mode_pwm_phasecorrect_until_0x00FF();
-inline void timer1_mode_pwm_phasecorrect_until_0x01FF();
-inline void timer1_mode_pwm_phasecorrect_until_0x03FF();
-inline void timer1_mode_ctc_until_OCR1A();
-inline void timer1_mode_pwm_fast_until_0x00FF();
-inline void timer1_mode_pwm_fast_until_0x01FF();
-inline void timer1_mode_pwm_fast_until_0x03FF();
-inline void timer1_mode_pwm_phasefrequencycorrect_until_ICR1();
-inline void timer1_mode_pwm_phasefrequencycorrect_until_OCR1A();
-inline void timer1_mode_pwm_phasecorrect_until_ICR1();
-inline void timer1_mode_pwm_phasecorrect_until_OCR1A();
-inline void timer1_mode_ctc_until_ICR1();
-inline void timer1_mode_pwm_fast_until_ICR1();
-inline void timer1_mode_pwm_fast_until_OCR1A();
+
+inline void timer1_mode_normal()
+{
+	TCCR1B &= ~(1<<WGM13);
+	TCCR1B &= ~(1<<WGM12);
+	TCCR1A &= ~(1<<WGM11);
+	TCCR1A &= ~(1<<WGM10);
+}
+
+inline void timer1_mode_pwm_phasecorrect_until_0x00FF()
+{
+	TCCR1B &= ~(1<<WGM13);
+	TCCR1B &= ~(1<<WGM12);
+	TCCR1A &= ~(1<<WGM11);
+	TCCR1A |=  (1<<WGM10);
+}
+
+inline void timer1_mode_pwm_phasecorrect_until_0x01FF()
+{
+	TCCR1B &= ~(1<<WGM13);
+	TCCR1B &= ~(1<<WGM12);
+	TCCR1A |=  (1<<WGM11);
+	TCCR1A &= ~(1<<WGM10);
+}
+
+inline void timer1_mode_pwm_phasecorrect_until_0x03FF()
+{
+	TCCR1B &= ~(1<<WGM13);
+	TCCR1B &= ~(1<<WGM12);
+	TCCR1A |=  (1<<WGM11);
+	TCCR1A |=  (1<<WGM10);
+}
+
+inline void timer1_mode_ctc_until_OCR1A()
+{
+	TCCR1B &= ~(1<<WGM13);
+	TCCR1B |=  (1<<WGM12);
+	TCCR1A &= ~(1<<WGM11);
+	TCCR1A &= ~(1<<WGM10);
+}
+
+inline void timer1_mode_pwm_fast_until_0x00FF()
+{
+	TCCR1B &= ~(1<<WGM13);
+	TCCR1B |=  (1<<WGM12);
+	TCCR1A &= ~(1<<WGM11);
+	TCCR1A |=  (1<<WGM10);
+}
+
+inline void timer1_mode_pwm_fast_until_0x01FF()
+{
+	TCCR1B &= ~(1<<WGM13);
+	TCCR1B |=  (1<<WGM12);
+	TCCR1A |=  (1<<WGM11);
+	TCCR1A &= ~(1<<WGM10);
+}
+
+inline void timer1_mode_pwm_fast_until_0x03FF()
+{
+	TCCR1B &= ~(1<<WGM13);
+	TCCR1B |=  (1<<WGM12);
+	TCCR1A |=  (1<<WGM11);
+	TCCR1A |=  (1<<WGM10);
+}
+
+inline void timer1_mode_pwm_phasefrequencycorrect_until_ICR1()
+{
+	TCCR1B |=  (1<<WGM13);
+	TCCR1B &= ~(1<<WGM12);
+	TCCR1A &= ~(1<<WGM11);
+	TCCR1A &= ~(1<<WGM10);
+}
+
+inline void timer1_mode_pwm_phasefrequencycorrect_until_OCR1A()
+{
+	TCCR1B |=  (1<<WGM13);
+	TCCR1B &= ~(1<<WGM12);
+	TCCR1A &= ~(1<<WGM11);
+	TCCR1A |=  (1<<WGM10);
+}
+
+inline void timer1_mode_pwm_phasecorrect_until_ICR1()
+{
+	TCCR1B |=  (1<<WGM13);
+	TCCR1B &= ~(1<<WGM12);
+	TCCR1A |=  (1<<WGM11);
+	TCCR1A &= ~(1<<WGM10);
+}
+
+inline void timer1_mode_pwm_phasecorrect_until_OCR1A()
+{
+	TCCR1B |=  (1<<WGM13);
+	TCCR1B &= ~(1<<WGM12);
+	TCCR1A |=  (1<<WGM11);
+	TCCR1A |=  (1<<WGM10);
+}
+
+inline void timer1_mode_ctc_until_ICR1()
+{
+	TCCR1B |=  (1<<WGM13);
+	TCCR1B |=  (1<<WGM12);
+	TCCR1A &= ~(1<<WGM11);
+	TCCR1A &= ~(1<<WGM10);
+}
+
+inline void timer1_mode_pwm_fast_until_ICR1()
+{
+	TCCR1B |=  (1<<WGM13);
+	TCCR1B |=  (1<<WGM12);
+	TCCR1A |=  (1<<WGM11);
+	TCCR1A &= ~(1<<WGM10);
+}
+
+inline void timer1_mode_pwm_fast_until_OCR1A()
+{
+	TCCR1B |=  (1<<WGM13);
+	TCCR1B |=  (1<<WGM12);
+	TCCR1A |=  (1<<WGM11);
+	TCCR1A |=  (1<<WGM10);
+}
+
 // timer1_clockselect
-inline void timer1_clockselect_none();
-inline void timer1_clockselect_io_1();
-inline void timer1_clockselect_io_8();
-inline void timer1_clockselect_io_64();
-inline void timer1_clockselect_io_256();
-inline void timer1_clockselect_io_1024();
-inline void timer1_clockselect_on_falling_T1();
-inline void timer1_clockselect_on_rising_T1();
+
+inline void timer1_clockselect_none()
+{
+	TCCR1B &= ~(1<<CS12);
+	TCCR1B &= ~(1<<CS11);
+	TCCR1B &= ~(1<<CS10);
+}
+
+inline void timer1_clockselect_io_1()
+{
+	TCCR1B &= ~(1<<CS12);
+	TCCR1B &= ~(1<<CS11);
+	TCCR1B |=  (1<<CS10);
+}
+
+inline void timer1_clockselect_io_8()
+{
+	TCCR1B &= ~(1<<CS12);
+	TCCR1B |=  (1<<CS11);
+	TCCR1B &= ~(1<<CS10);
+}
+
+inline void timer1_clockselect_io_64()
+{
+	TCCR1B &= ~(1<<CS12);
+	TCCR1B |=  (1<<CS11);
+	TCCR1B |=  (1<<CS10);
+}
+
+inline void timer1_clockselect_io_256()
+{
+	TCCR1B |=  (1<<CS12);
+	TCCR1B &= ~(1<<CS11);
+	TCCR1B &= ~(1<<CS10);
+}
+
+inline void timer1_clockselect_io_1024()
+{
+	TCCR1B |=  (1<<CS12);
+	TCCR1B &= ~(1<<CS11);
+	TCCR1B |=  (1<<CS10);
+}
+
+inline void timer1_clockselect_on_falling_T1()
+{
+	TCCR1B |=  (1<<CS12);
+	TCCR1B |=  (1<<CS11);
+	TCCR1B &= ~(1<<CS10);
+}
+
+inline void timer1_clockselect_on_rising_T1()
+{
+	TCCR1B |=  (1<<CS12);
+	TCCR1B |=  (1<<CS11);
+	TCCR1B |=  (1<<CS10);
+}
+
+// timer1_out_A
+
+inline void timer1_out_A_no_pwm()
+{
+	TCCR1A &= ~(1<<COM1A1);
+	TCCR1A &= ~(1<<COM1A0);
+}
+
+inline void timer1_out_A_clear_on_compare_match()
+{
+	TCCR1A |=  (1<<COM1A1);
+	TCCR1A &= ~(1<<COM1A0);
+}
+
+inline void timer1_out_A_set_on_compare_match()
+{
+	TCCR1A |=  (1<<COM1A1);
+	TCCR1A |=  (1<<COM1A0);
+}
+
+// timer1_out_B
+
+inline void timer1_out_B_no_pwm()
+{
+	TCCR1A &= ~(1<<COM1B1);
+	TCCR1A &= ~(1<<COM1B0);
+}
+
+inline void timer1_out_B_clear_on_compare_match()
+{
+	TCCR1A |=  (1<<COM1B1);
+	TCCR1A &= ~(1<<COM1B0);
+}
+
+inline void timer1_out_B_set_on_compare_match()
+{
+	TCCR1A |=  (1<<COM1B1);
+	TCCR1A |=  (1<<COM1B0);
+}
+
 // timer2_mode
-inline void timer2_mode_normal();
-inline void timer2_mode_pwm_phasecorrect_until_0xFF();
-inline void timer2_mode_ctc_until_OCR2A();
-inline void timer2_mode_pwm_fast_until_0xFF();
-inline void timer2_mode_pwm_phasecorrect_until_OCR2A();
-inline void timer2_mode_pwm_fast_until_OCR2A();
+
+inline void timer2_mode_normal()
+{
+	TCCR2B &= ~(1<<WGM22);
+	TCCR2A &= ~(1<<WGM21);
+	TCCR2A &= ~(1<<WGM20);
+}
+
+inline void timer2_mode_pwm_phasecorrect_until_0xFF()
+{
+	TCCR2B &= ~(1<<WGM22);
+	TCCR2A &= ~(1<<WGM21);
+	TCCR2A |=  (1<<WGM20);
+}
+
+inline void timer2_mode_ctc_until_OCR2A()
+{
+	TCCR2B &= ~(1<<WGM22);
+	TCCR2A |=  (1<<WGM21);
+	TCCR2A &= ~(1<<WGM20);
+}
+
+inline void timer2_mode_pwm_fast_until_0xFF()
+{
+	TCCR2B &= ~(1<<WGM22);
+	TCCR2A |=  (1<<WGM21);
+	TCCR2A |=  (1<<WGM20);
+}
+
+inline void timer2_mode_pwm_phasecorrect_until_OCR2A()
+{
+	TCCR2B |=  (1<<WGM22);
+	TCCR2A &= ~(1<<WGM21);
+	TCCR2A |=  (1<<WGM20);
+}
+
+inline void timer2_mode_pwm_fast_until_OCR2A()
+{
+	TCCR2B |=  (1<<WGM22);
+	TCCR2A |=  (1<<WGM21);
+	TCCR2A |=  (1<<WGM20);
+}
+
 // timer2_clockselect
-inline void timer2_clockselect_none();
-inline void timer2_clockselect_t2clk_1();
-inline void timer2_clockselect_t2clk_8();
-inline void timer2_clockselect_t2clk_32();
-inline void timer2_clockselect_t2clk_64();
-inline void timer2_clockselect_t2clk_128();
-inline void timer2_clockselect_t2clk_256();
-inline void timer2_clockselect_t2clk_1024();
-// spi0_enable
-inline void spi0_enable_disable();
-inline void spi0_enable_enable();
-// spi0_mode
-inline void spi0_mode_slave();
-inline void spi0_mode_master();
-// spi0_polarity
-inline void spi0_polarity_leading_rising();
-inline void spi0_polarity_leading_falling();
-// spi0_phase
-inline void spi0_phase_leading_sample();
-inline void spi0_phase_leading_setup();
-// spi0_clock
-inline void spi0_clock_fosc_4();
-inline void spi0_clock_fosc_16();
-inline void spi0_clock_fosc_64();
-inline void spi0_clock_fosc_128();
-inline void spi0_clock_fosc_2();
-inline void spi0_clock_fosc_8();
-inline void spi0_clock_fosc_32();
-inline void spi0_clock_fosc_64();
+
+inline void timer2_clockselect_none()
+{
+	TCCR2B &= ~(1<<CS22);
+	TCCR2B &= ~(1<<CS21);
+	TCCR2B &= ~(1<<CS20);
+}
+
+inline void timer2_clockselect_t2clk_1()
+{
+	TCCR2B &= ~(1<<CS22);
+	TCCR2B &= ~(1<<CS21);
+	TCCR2B |=  (1<<CS20);
+}
+
+inline void timer2_clockselect_t2clk_8()
+{
+	TCCR2B &= ~(1<<CS22);
+	TCCR2B |=  (1<<CS21);
+	TCCR2B &= ~(1<<CS20);
+}
+
+inline void timer2_clockselect_t2clk_32()
+{
+	TCCR2B &= ~(1<<CS22);
+	TCCR2B |=  (1<<CS21);
+	TCCR2B |=  (1<<CS20);
+}
+
+inline void timer2_clockselect_t2clk_64()
+{
+	TCCR2B |=  (1<<CS22);
+	TCCR2B &= ~(1<<CS21);
+	TCCR2B &= ~(1<<CS20);
+}
+
+inline void timer2_clockselect_t2clk_128()
+{
+	TCCR2B |=  (1<<CS22);
+	TCCR2B &= ~(1<<CS21);
+	TCCR2B |=  (1<<CS20);
+}
+
+inline void timer2_clockselect_t2clk_256()
+{
+	TCCR2B |=  (1<<CS22);
+	TCCR2B |=  (1<<CS21);
+	TCCR2B &= ~(1<<CS20);
+}
+
+inline void timer2_clockselect_t2clk_1024()
+{
+	TCCR2B |=  (1<<CS22);
+	TCCR2B |=  (1<<CS21);
+	TCCR2B |=  (1<<CS20);
+}
+
 // interrupts
-inline void int_timer0_compA_enable();
-inline void int_timer0_compA_disable();
-inline void int_timer0_compB_enable();
-inline void int_timer0_compB_disable();
-inline void int_timer0_overflow_enable();
-inline void int_timer0_overflow_disable();
-inline void int_timer1_input_capture_enable();
-inline void int_timer1_input_capture_disable();
-inline void int_timer1_compA_enable();
-inline void int_timer1_compA_disable();
-inline void int_timer1_compB_enable();
-inline void int_timer1_compB_disable();
-inline void int_timer1_overflow_enable();
-inline void int_timer1_overflow_disable();
-inline void int_spi0_enable();
-inline void int_spi0_disable();
+
+
+inline void int_timer0_compA_enable()
+{
+	TIMSK0 |=  (1<<OCIE0A);
+}
+
+inline void int_timer0_compA_disable()
+{
+	TIMSK0 &= ~(1<<OCIE0A);
+}
+
+inline void int_timer0_compB_enable()
+{
+	TIMSK0 |=  (1<<OCIE0B);
+}
+
+inline void int_timer0_compB_disable()
+{
+	TIMSK0 &= ~(1<<OCIE0B);
+}
+
+inline void int_timer0_overflow_enable()
+{
+	TIMSK0 |=  (1<<TOIE0);
+}
+
+inline void int_timer0_overflow_disable()
+{
+	TIMSK0 &= ~(1<<TOIE0);
+}
+
+inline void int_timer1_input_capture_enable()
+{
+	TIMSK1 |=  (1<<ICIE1);
+}
+
+inline void int_timer1_input_capture_disable()
+{
+	TIMSK1 &= ~(1<<ICIE1);
+}
+
+inline void int_timer1_compA_enable()
+{
+	TIMSK1 |=  (1<<OCIE1A);
+}
+
+inline void int_timer1_compA_disable()
+{
+	TIMSK1 &= ~(1<<OCIE1A);
+}
+
+inline void int_timer1_compB_enable()
+{
+	TIMSK1 |=  (1<<OCIE1B);
+}
+
+inline void int_timer1_compB_disable()
+{
+	TIMSK1 &= ~(1<<OCIE1B);
+}
+
+inline void int_timer1_overflow_enable()
+{
+	TIMSK1 |=  (1<<TOIE1);
+}
+
+inline void int_timer1_overflow_disable()
+{
+	TIMSK1 &= ~(1<<TOIE1);
+}
